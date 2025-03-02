5c6023f43509d63c61cc6c8021e0349ba712afb1
Jamie787 <jamiesiennagold@gmail.com>
2025-03-02 17:15:27 +1100

    Wrote the backend code

--- backend/ts_template/devdonalds.ts
+++ backend/ts_template/devdonalds.ts
@@ -1,9 +1,11 @@
 import express, { Request, Response } from "express";
 
 // ==== Type Definitions, feel free to add or modify ==========================
+type CookBookTypes = 'recipe' | 'ingredient';
+
 interface cookbookEntry {
   name: string;
-  type: string;
+  type: CookBookTypes;
 }
 
 interface requiredItem {
@@ -19,6 +21,12 @@ interface ingredient extends cookbookEntry {
   cookTime: number;
 }
 
+interface recipeSummary {
+  name: string;
+  cookTime: number;
+  ingredients: requiredItem[];
+}
+
 // =============================================================================
 // ==== HTTP Endpoint Stubs ====================================================
 // =============================================================================
@@ -26,7 +34,7 @@ const app = express();
 app.use(express.json());
 
 // Store your recipes here!
-const cookbook: any = null;
+const cookbook: (recipe | ingredient)[] = [];
 
 // Task 1 helper (don't touch)
 app.post("/parse", (req:Request, res:Response) => {
@@ -45,25 +53,138 @@ app.post("/parse", (req:Request, res:Response) => {
 // [TASK 1] ====================================================================
 // Takes in a recipeName and returns it in a form that 
 const parse_handwriting = (recipeName: string): string | null => {
-  // TODO: implement me
-  return recipeName
+  recipeName = recipeName.toLowerCase();
+  recipeName = recipeName.replace(/[-_]+/g, ' ');
+  recipeName = recipeName.replace(/[^A-Za-z ]+/g, '');
+  recipeName = recipeName.replace(/\s+/g,' ').trim();
+
+  if (recipeName.length <= 0) {
+    return null;
+  }
+
+  let names = recipeName.split(" ");
+  names = names.map((name) => name[0].toUpperCase() + name.slice(1));
+  recipeName = names.reduce((recipe, currentName) => recipe + ' ' + currentName);
+  recipeName = recipeName.trim();
+
+  return recipeName;
 }
 
 // [TASK 2] ====================================================================
 // Endpoint that adds a CookbookEntry to your magical cookbook
 app.post("/entry", (req:Request, res:Response) => {
-  // TODO: implement me
-  res.status(500).send("not yet implemented!")
+  try {
+    switch (req.body.type) {
+      case 'recipe':
+        addRecipe(req.body.name, req.body.requiredItems);
+        break;
+    
+      case 'ingredient':
+        addIngredient(req.body.name, req.body.cookTime);
+        break;
+  
+      default:
+        return res.status(400).send('Invalid type');
+    }
 
+    return res.json({});
+  } catch (error) {
+    return res.status(400).send(error.message);
+  }
 });
+
+function addRecipe(name: string, requiredItems: requiredItem[]) {
+  checkDuplicateName(name);
+
+  const recipe: recipe = { type: 'recipe', name: name, requiredItems: requiredItems };
+
+  for (let i = 0; i < requiredItems.length; i++) {
+    for (let j = i + 1; j < requiredItems.length; j++) {
+      if (requiredItems[j].name === requiredItems[i].name) {
+        throw new Error('Duplicate item name');
+      }
+    }
+  }
+
+  cookbook.push(recipe);
+}
+
+function addIngredient(name: string, cookTime: number) {
+  checkDuplicateName(name);
+
+  if (cookTime < 0) {
+    throw new Error('Invalid cooktime');
+  }
+
+  cookbook.push({type: 'ingredient', name: name, cookTime: cookTime});
+}
+
+function checkDuplicateName(name: string) {
+  cookbook.forEach((entry) => {
+    if (entry.name === name) {
+      throw new Error('Duplicate name');
+    }
+  });
+}
 
 // [TASK 3] ====================================================================
 // Endpoint that returns a summary of a recipe that corresponds to a query name
 app.get("/summary", (req:Request, res:Request) => {
-  // TODO: implement me
-  res.status(500).send("not yet implemented!")
-
+  try {
+    res.json(getRecipeSummary(req.query.name as string));
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
 });
+
+function getRecipeSummary(name: string): recipeSummary {
+  const recipe = cookbook.find((item) => item.name === name) as recipe;
+  if (!recipe) {
+    throw new Error(`${name} is not in the cookbook`);
+  } else if (recipe.type === 'ingredient') {
+    throw new Error(`${name} is an ingredient, not a recipe`);
+  }
+  
+  const summary: recipeSummary = { name: name, cookTime: 0, ingredients: []};
+
+  for (const item of recipe.requiredItems) {
+    const itemEntry = cookbook.find((entry) => entry.name === item.name) as ingredient;
+    if (!itemEntry) {
+      throw new Error(`${item.name} is not in the cookbook`);
+    }
+
+    if (itemEntry.type === 'recipe') {
+      const itemRecipeSummary = getRecipeSummary(itemEntry.name);
+
+      summary.cookTime += itemRecipeSummary.cookTime * item.quantity;
+      const itemIngrediants = itemRecipeSummary.ingredients.map((summaryItem) => 
+        ({name: summaryItem.name, quantity: summaryItem.quantity * item.quantity}));
+      addRequiredItems(itemIngrediants, summary.ingredients);
+      
+    } else {
+      summary.cookTime += item.quantity * itemEntry.cookTime;
+      addRequiredItems([item], summary.ingredients);
+    }
+  }
+
+  return summary;
+}
+
+function addRequiredItems(src: requiredItem[], dest: requiredItem[]) {
+  for (const item of src) {
+    const destItem = dest.find((destElement) => destElement.name === item.name);
+    
+    if (!destItem) {
+      dest.push(item);
+    } else {
+      destItem.quantity += item.quantity;
+    }
+  }
+}
+
+app.get('/data', (req, res) => {
+  res.json(cookbook);
+})
 
 // =============================================================================
 // ==== DO NOT TOUCH ===========================================================
